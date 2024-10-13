e139b20deb914daaaa020d597ce4144f9ba1309a
danula2008 <danusathmin@gmail.com>
2024-10-13 22:58:46 +0530

    first commit

---
+++ src/main/java/icet/edu/erp/Main.java
@@ -0,0 +1,7 @@
+package icet.edu.erp;
+
+public class Main {
+    public static void main(String[] args) {
+        Starter.main(args);
+    }
+}
---
+++ src/main/java/icet/edu/erp/Starter.java
@@ -0,0 +1,16 @@
+package icet.edu.erp;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+
+public class Starter extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
